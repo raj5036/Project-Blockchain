775faac67f7101b34db22a4b41eb4670826ed467
Raj <97rajmath@gmail.com>
2021-03-07 09:38:56 -0500

    updated

--- Code/blockchain.py
+++ Code/blockchain.py
@@ -11,9 +11,9 @@ second_block = Block(genesis_block.block_hash, ["transaction4", "transaction5"])
 third_block = Block(second_block.block_hash, ["transaction6", "transaction7", "transaction8"])
 
 
-print("Hash of genesis block is {}".format(hash1))
+print("Hash of genesis block is {}".format(genesis_block.block_hash))
 
-print("Hash of second block is {}".format(genesis_block.block_hash))
+print("Hash of second block is {}".format(second_block.block_hash))
 
 print("Hash of third block is {}".format(third_block.block_hash))
 
