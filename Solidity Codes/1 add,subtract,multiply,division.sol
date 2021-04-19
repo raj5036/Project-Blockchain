9a1d337796f8cee76930168693f2a0fde95546a8
Raj <97rajmath@gmail.com>
2021-04-19 09:58:52 -0500

    Added the first Solidity Codes

---
+++ Solidity Codes/1 add,subtract,multiply,division.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.24;
+
+contract MyContract{
+    int a;
+    int b;
+    
+    function set(int _a,int _b) public{
+        a=_a;
+        b=_b;
+    }
+    
+    function add() public view returns(int){
+        return a+b;
+    }
+    
+    function subtract() public view returns(int){
+        return a-b;
+    }
+    
+    function multiply() public view returns(int){
+        return a-b;
+    }
+    
+    function division() public view returns(int){
+        return a/b;
+    }
+}
